72f649cae9f050fe8fc35c6bf5c130c9bca2e45b
japygo <hyunnjinn@gmail.com>
2025-12-04 17:43:40 +0900

    feat: Add Coil library and configure network permissions
    Adds the Coil library for image loading and configures the necessary network settings.
    
    - Adds the `coil-compose` and `coil-network-okhttp` dependencies to `build.gradle.kts`.
    - Defines the Coil version in `gradle/libs.versions.toml`.
    - Adds the `INTERNET` permission and enables `usesCleartextTraffic` in `AndroidManifest.xml` to allow network operations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
