a4b3e255023bae9cfa489af6645b5ad066d7b987
japygo <hyunnjinn@gmail.com>
2025-12-19 17:16:25 +0900

    refactor: DI 모듈의 NetworkMonitor import 경로 수정
    - util 패키지 리팩터링에 따른 import 경로를 core.util로 변경
    - VersionModule 내 QA 및 PROD flavor에서 동일한 수정 적용
    - DI 설정 참조 경로 오류 문제 해결 및 코드 정리

--- app/src/prod/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/VersionModule.kt
+++ app/src/prod/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/VersionModule.kt
@@ -1,7 +1,7 @@
 package com.survivalcoding.gangnam2kiandroidstudy.core.di
 
-import com.survivalcoding.gangnam2kiandroidstudy.util.NetworkMonitor
-import com.survivalcoding.gangnam2kiandroidstudy.util.RealNetworkMonitor
+import com.survivalcoding.gangnam2kiandroidstudy.core.util.NetworkMonitor
+import com.survivalcoding.gangnam2kiandroidstudy.core.util.RealNetworkMonitor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
--- app/src/qa/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/VersionModule.kt
+++ app/src/qa/java/com/survivalcoding/gangnam2kiandroidstudy/core/di/VersionModule.kt
@@ -1,7 +1,7 @@
 package com.survivalcoding.gangnam2kiandroidstudy.core.di
 
-import com.survivalcoding.gangnam2kiandroidstudy.util.NetworkMonitor
-import com.survivalcoding.gangnam2kiandroidstudy.util.OnlineNetworkMonitor
+import com.survivalcoding.gangnam2kiandroidstudy.core.util.NetworkMonitor
+import com.survivalcoding.gangnam2kiandroidstudy.core.util.OnlineNetworkMonitor
 import org.koin.dsl.module
 
 val versionModule = module {
