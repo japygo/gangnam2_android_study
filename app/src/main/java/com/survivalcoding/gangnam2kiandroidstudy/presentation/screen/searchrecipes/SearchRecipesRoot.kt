ad714d8ab425ac386bd8ad60fc07692ab74cdedf
japygo <hyunnjinn@gmail.com>
2025-12-23 17:07:15 +0900

    refactor: SearchRecipesRoot의 BackHandler 로직 수정 및 상태 관리 개선
    - backHandlingEnabled 변수 추가로 BackHandler 활성화 상태 제어
    - Navigation 이벤트 처리를 위해 OnDismissRequest 대신 onNavigate 호출
    - 기존 코드의 상태 관리 방식 개선으로 가독성과 유지보수성 강화

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/searchrecipes/SearchRecipesRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/searchrecipes/SearchRecipesRoot.kt
@@ -18,7 +18,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -50,8 +52,9 @@ fun SearchRecipesRoot(
         }
     }
 
-    BackHandler(uiState.isSheetVisible) {
-        viewModel.onAction(FilterAction.OnDismissRequest)
+    var backHandlingEnabled by remember { mutableStateOf(true) }
+    BackHandler(backHandlingEnabled) {
+        onNavigate(SearchRecipeNavigation.Back)
     }
 
     Scaffold(
